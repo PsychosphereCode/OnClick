c0f1c98d0f12882486e5d34b101bde76fa97bbf7
PsychosphereCode <fabian.rottens@gmail.com>
2022-02-20 16:38:00 -0300

    OnClick intento 4

---
+++ script.js
@@ -0,0 +1,11 @@
+function likeAlert(ninjaId) {
+    alert("Ninja was liked");
+}
+
+function handleLogin() {
+    document.getElementById("id_btn").innerText = "Log Out"
+}
+
+function deleteBtn(param) {
+    param.style="visibility: hidden;";
+}
